22c07c9b1e1ecd0455f6625ad02a79b0ab0fffec
Gabriel Teixeira araujo <gateixei@c2r8s1.42lisboa.com>
2023-01-25 11:36:55 +0000

    Validation and 0 to 5 number ok

--- utils.c
+++ utils.c
@@ -44,16 +44,6 @@ int get_max(stack** root)
     return (i);
 }
 
-int ft_strlen(char* str)
-{
-    int i;
-
-    i = 0;
-    while (str[i] != '\0')
-        i++;
-    return (i);
-}
-
 void ft_print_error(void)
 {
 	write(1, "Error\n", 6);
